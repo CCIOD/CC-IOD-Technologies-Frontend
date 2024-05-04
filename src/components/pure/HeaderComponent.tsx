e4bb14dd6342f6f5f846d768a06b92901030adfc
VictorManuel2 <victorortizalvarez2530@gmail.com>
2024-05-03 20:22:19 -0600

    dark and light theme

--- src/components/pure/HeaderComponent.tsx
+++ src/components/pure/HeaderComponent.tsx
@@ -1,29 +1,57 @@
-import { useContext } from "react";
-import { RxHamburgerMenu } from "react-icons/rx"
+import { useContext, useEffect, useState } from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
 import { SidebarContext } from "../../context/SidebarContext";
 import { RiLogoutBoxRLine } from "react-icons/ri";
 import { Button } from "./Button";
+import { FiMoon, FiSun } from "react-icons/fi";
 
 export const HeaderComponent = () => {
+  const [theme, setTheme] = useState(() => {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      return "dark";
+    }
+    return "light";
+  });
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.querySelector("html")?.classList.add("dark");
+    } else {
+      document.querySelector("html")?.classList.remove("dark");
+    }
+  }, [theme]);
+
+  const handleChangeTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
   const { toggleSidebarMobile } = useContext(SidebarContext);
   const handleToggleMenu = () => toggleSidebarMobile();
-  const handleClick = () => {}
+  const handleClick = () => {};
   return (
-    <div className='app-bg app-text h-14 flex gap-4 items-center justify-between md:justify-end px-5'>
+    <div className="app-bg app-text h-14 flex gap-4 items-center justify-between md:justify-end px-5">
       <div className="block md:hidden">
-<button onClick={() => handleToggleMenu()}><RxHamburgerMenu size={24} /></button>
+        <button onClick={() => handleToggleMenu()}>
+          <RxHamburgerMenu size={24} />
+        </button>
       </div>
       <div className="flex gap-4 items-center">
-
-      {/* <div></div> Toggle change Theme */}
-      <div>
-        <span>Víctor Manuel</span>
-      </div>
+        {/* <div></div> Toggle change Theme */}
+        <div>
+          <button
+            onClick={handleChangeTheme}
+          >
+            {theme === "light" ? <FiMoon className="mr-2" size={24}/> : <FiSun className="mr-2" size={24}/> }
+          </button>
+        </div>
+        <div>
+          <span>Víctor Manuel</span>
+        </div>
         <Button handleClick={handleClick}>
           <span className="hidden sm:block">Cerrar sesión</span>
           <RiLogoutBoxRLine className="block sm:hidden" size={24} />
         </Button>
       </div>
     </div>
-  )
-}
+  );
+};
--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,32 +1,34 @@
-import { RiAtFill } from "react-icons/ri"
+import { MdOutlineEmail } from "react-icons/md"
+import { RiLockPasswordLine } from "react-icons/ri"
 import { NavLink } from "react-router-dom"
 
 export const SignInPage = () => {
   return (
     <div className="h-screen flex justify-center items-center bg-no-repeat bg-cover bg-center" style={{ backgroundImage: "url('/src/assets/brazalete-login.jpeg')" }}>
       <div className="absolute inset-0 bg-cciod-black-300 bg-opacity-70 flex justify-center items-center">
-        <div className="w-2/5 flex flex-col md:flex-row">
-          <div className="w-3/5 p-7 bg-blue-900 text-cciod-white-200 rounded-l-lg flex flex-col justify-evenly items-center">
+        <div className="w-4/5 h-full flex flex-col justify-center items-center md:flex-row md:w-2/5">
+          <div className="w-3/4 h-1/4 p-7 bg-blue-900 text-cciod-white-200 rounded-t-lg flex flex-col justify-evenly items-center md:w-3/5 md:h-2/4  md:rounded-none md:rounded-l-lg">
             <img src="src/assets/Logo-CC-IOD.png" alt="logo" className="-mt-6" />
 
-            <h2 className="font-bold text-4xl">Bienvenidos</h2>
-            <p className="text-xl text-center">Para comenzar, por favor ingrese sus datos.</p>
+            <h2 className="font-bold text-3xl md:text-4xl">Bienvenidos</h2>
+            <p className="text-base text-center mt-4 md:text-lg">Para comenzar, por favor ingrese sus datos.</p>
           </div>
 
-          <div className="w-3/4 bg-white rounded-r-lg p-8">
-            <form action="" className="w-full max-w-md flex flex-col items-center">
-              <h2 className="text-4xl font-bold text-blue-900 mb-8 mt-6 text-center">Crea una Cuenta</h2>
+          <div className="w-3/4 h-2/4 bg-white rounded-b-lg p-8 md:h-2/4 md:rounded-none md:rounded-r-lg md:w-3/4">
+            <form action="" className="w-full max-w-md flex flex-col">
+              <h2 className="text-4xl font-bold text-blue-900 mb-4 mt-4 text-center md:mt-10">Iniciar Sesión</h2>
               <div className="relative w-full">
-                <RiAtFill className="absolute top-1/4 right-2" size={20} />
-                <input type="text" placeholder="Nombre de Usuario" className="p-2 w-full rounded text-black border border-black" />
+                <MdOutlineEmail className="absolute top-2/4 right-2" size={24}/>
+                <input type="email" placeholder="example@gmail.com" className="mt-6 p-2 w-full rounded border border-black" />
               </div>
-              <input type="email" placeholder="example@gmail.com" className="mt-6 p-2 w-full rounded border border-black" />
-              <input type="password" placeholder="****" className="mt-6 p-2 w-full rounded border border-black" />
-
-              <input type="submit" className="bg-blue-900 p-2 w-full mt-10 font-medium rounded text-cciod-white-200 cursor-pointer" value="INICIAR SESIÓN" />
+              <div className="relative w-full">
+                <RiLockPasswordLine className="absolute top-2/4 right-2" size={24}/>
+                <input type="password" placeholder="********" className="mt-6 p-2 w-full rounded border border-black" />
+              </div>
+              <input type="submit" className="bg-blue-900 p-2 w-full mt-10 font-medium rounded text-cciod-white-200 cursor-pointer" value="INGRESAR" />
               <NavLink
                 to="/recover-password"
-                className="mt-4"
+                className="mt-8 text-center"
               >
                 ¿Olvidaste tu contraseña?
               </NavLink>
