66d83a02750ca93ed87bdd52a879534ff08bb92a
JCarlosrm <juanc.ramirezm16@gmail.com>
2024-05-17 21:38:01 -0600

    Personalizar boton de archivo | Alertas de respuesta | permitir la eliminación de archivos

--- src/components/modalForms/OperationForm.tsx
+++ src/components/modalForms/OperationForm.tsx
@@ -1,39 +1,86 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import { Button } from "../generic/Button";
 import { InputFile } from "../Inputs/InputFile";
 import { Form, Formik } from "formik";
 import { operationSchema } from "../../utils/FormSchema";
-import { IOperationForm } from "../../interfaces/operations.interface";
+import {
+  DataRowOperations,
+  IOperationForm,
+} from "../../interfaces/operations.interface";
+import { ResetInputFile } from "../Inputs/ResetInputFile";
+import { deleteFileFromApi } from "../../services/operationsService";
+import { ApiResponse } from "../../interfaces/response.interface";
+import { alertTimer } from "../../utils/alerts";
 
 type Props = {
   toggleModal: (param: boolean) => void;
   handleSubmit: (data: IOperationForm) => void;
   btnText: "Guardar";
+  operationData: DataRowOperations | null;
+  toggleAction: () => void;
 };
 export const OperationForm: FC<Props> = ({
   toggleModal,
   handleSubmit,
   btnText,
+  operationData,
+  toggleAction,
 }) => {
-  const [contractInput, setContractInput] = useState("Selecciona un contrato");
-  const [reportInput, setReportInput] = useState("Selecciona un reporte");
+  const [contractInput, setContractInput] = useState<string>(
+    "Selecciona un contrato"
+  );
+  const [reportInput, setReportInput] = useState<string>(
+    "Selecciona un reporte"
+  );
+  const [isContract, setIsContract] = useState<boolean>(false);
+  const [isReport, setIsReport] = useState<boolean>(false);
 
   const initialData: IOperationForm = {
     contract: null,
     installation_report: null,
   };
 
+  useEffect(() => {
+    if (operationData) {
+      setIsContract(operationData.contract ? true : false);
+      setIsReport(operationData.installation_report ? true : false);
+    }
+  }, [operationData]);
+
+  const handleDeleteFile = async (file: "contract" | "installation_report") => {
+    try {
+      const res = await deleteFileFromApi(operationData!.id, file);
+      if (res.success) {
+        file === "contract" ? setIsContract(false) : setIsReport(false);
+        const name =
+          file === "contract" ? "contrato" : "reporte de instalación";
+        toggleAction();
+        alertTimer(`El ${name} se ha eliminado.`, "success");
+      }
+    } catch (error) {
+      const err = error as ApiResponse;
+      alertTimer(err.message, "error");
+    }
+  };
+
   return (
-    <>
-      <div className="h-full py-2 flex flex-col justify-between">
-        <div className="w-full h-full">
-          <Formik
-            initialValues={initialData}
-            validationSchema={operationSchema}
-            onSubmit={(data) => handleSubmit(data)}
-          >
-            {({ setFieldValue }) => (
-              <Form className="w-full flex flex-col gap-4">
+    <div className="h-full py-2 flex flex-col justify-between">
+      <div className="w-full h-full">
+        <Formik
+          initialValues={initialData}
+          validationSchema={operationSchema}
+          onSubmit={(data) => handleSubmit(data)}
+        >
+          {({ setFieldValue }) => (
+            <Form className="w-full flex flex-col gap-4">
+              {isContract ? (
+                <ResetInputFile
+                  filename={
+                    operationData?.contract ? operationData?.contract : ""
+                  }
+                  handleClick={() => handleDeleteFile("contract")}
+                />
+              ) : (
                 <InputFile
                   name="contract"
                   type="file"
@@ -44,6 +91,17 @@ export const OperationForm: FC<Props> = ({
                     setFieldValue("contract", file);
                   }}
                 />
+              )}
+              {isReport ? (
+                <ResetInputFile
+                  filename={
+                    operationData?.installation_report
+                      ? operationData?.installation_report
+                      : ""
+                  }
+                  handleClick={() => handleDeleteFile("installation_report")}
+                />
+              ) : (
                 <InputFile
                   name="installation_report"
                   type="file"
@@ -54,6 +112,8 @@ export const OperationForm: FC<Props> = ({
                     setFieldValue("installation_report", file);
                   }}
                 />
+              )}
+              {(!isContract || !isReport) && (
                 <div className="flex justify-end gap-2">
                   <Button color="gray" onClick={() => toggleModal(false)}>
                     Cancelar
@@ -62,11 +122,12 @@ export const OperationForm: FC<Props> = ({
                     {btnText}
                   </Button>
                 </div>
-              </Form>
-            )}
-          </Formik>
-        </div>
+              )}
+            </Form>
+          )}
+        </Formik>
       </div>
-    </>
+    </div>
   );
 };
+// 136
--- src/pages/OperationsPage.tsx
+++ src/pages/OperationsPage.tsx
@@ -10,13 +10,18 @@ import {
 } from "../interfaces/operations.interface";
 import {
   getAllOperationsFromApi,
+  getOperationByIdFromApi,
   updateOperationFromApi,
 } from "../services/operationsService";
 import { FileDownload } from "../components/generic/FileDownload";
 import { ApiResponse } from "../interfaces/response.interface";
+import { alertTimer } from "../utils/alerts";
 
 export const OperationsPage = () => {
   const [operationsData, setOperationsData] = useState<DataRowOperations[]>([]);
+  const [operationData, setOperationData] = useState<DataRowOperations | null>(
+    null
+  );
   const [isLoading, setIsLoading] = useState(true);
   const [isOpenModal, setIsOpenModal] = useState<boolean>(false);
   const [titleModal, setTitleModal] = useState<string>("Documentos de");
@@ -28,6 +33,7 @@ export const OperationsPage = () => {
     setIsOpenModal(value);
     setOperationID(id);
   };
+  const toggleAction = () => setAction(!action);
 
   const columns: TableColumn<DataRowOperations>[] = [
     {
@@ -50,7 +56,11 @@ export const OperationsPage = () => {
       name: "Acciones",
       cell: (row) => (
         <TableActions
+          uploadFilesColor={
+            row.contract || row.installation_report ? "sky" : "green"
+          }
           handleUploadFiles={() => {
+            getOperationById(row.id);
             toggleModal(true, row.id);
             setTitleModal(`Archivos de ${row.name}`);
           }}
@@ -64,14 +74,23 @@ export const OperationsPage = () => {
     try {
       const res = await getAllOperationsFromApi();
       const data: DataRowOperations[] = res.data!;
-
       if (!data) setOperationsData([]);
       setOperationsData(data);
       setIsLoading(false);
-      console.log(data.sort());
+      setErrorMessage("");
+    } catch (error) {
+      setIsLoading(false);
+    }
+  };
+  const getOperationById = async (id: number) => {
+    try {
+      const res = await getOperationByIdFromApi(id);
+      const data: DataRowOperations = res.data!;
+
+      if (!data) setOperationsData([]);
+      setOperationData(data);
     } catch (error) {
       console.log(error);
-      setIsLoading(false);
     }
   };
   useEffect(() => {
@@ -86,13 +105,15 @@ export const OperationsPage = () => {
     const formData = new FormData();
     formData.append("contract", data.contract as File);
     formData.append("installation_report", data.installation_report as File);
-    console.log(formData.get("contract"));
 
     try {
       const res = await updateOperationFromApi(operationID as number, formData);
       console.log(res);
       toggleModal(false);
-      if (res.success) setAction(!action);
+      if (res.success) {
+        setAction(!action);
+        alertTimer(`La operación se ha actualizado`, "success");
+      }
     } catch (error) {
       const err = error as ApiResponse;
       if (err) setErrorMessage(err.message!);
@@ -120,6 +141,8 @@ export const OperationsPage = () => {
             toggleModal={toggleModal}
             btnText="Guardar"
             handleSubmit={(data) => handleUpload(data)}
+            operationData={operationData}
+            toggleAction={toggleAction}
           />
           {errorMessage && (
             <span className="block w-full mt-2 text-center text-sm text-red-500">
